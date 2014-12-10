85916d51aaeaae1a2e425fa8720a4ca4cf3c8c18
zquancai <zquancai@126.com>
2014-12-10 17:27:02 +0800

    完成滑块操作

--- js/home.js
+++ js/home.js
@@ -8,11 +8,12 @@
 
 var Scroll = function (ele) {
     var ele_dom, // 滑动的元素
+        pageN, // 滑块个数
         cwidth, // 单个页块的宽度
         dwidth, // 设备宽度
         curX = 0, // 当前的x坐标
         startX = 0, // 触摸起始的位置
-        curpage = 0,
+        curpage = 0, // 当前页面
         dir = 0 // 左右滑，-1左滑
         ;
     function _getPageX(event){
@@ -20,21 +21,28 @@ var Scroll = function (ele) {
         return a.pageX;
     }
     function _touchstart(event) {
+        ele_dom.className = 'scroll';
         startX = _getPageX(event);
     }
     function _touchmove(event) {
+        event.stopPropagation();
         curX = _getPageX(event);
-        _setX(curX - startX);
+        _setX(curX - startX - curpage * dwidth);
     }
     function _touchend(event) {
-        dir = curX - startX < 0 ? -1 : 1;
-        console.log(curX - startX);
-        if(dir == -1) {
-            curpage ++;
-            _moveTo(curpage);
+        var move = curX - startX;
+        if(Math.abs(move) >= 50) {
+            dir = move < 0 ? -1 : 1;
+            if (dir == -1) {
+                curpage = (curpage == pageN - 1 ? curpage : curpage + 1);
+                _moveTo(curpage);
+            }
+            else {
+                curpage = (curpage == 0 ? curpage : curpage - 1);
+                _moveTo(curpage);
+            }
         }
         else{
-            curpage = curpage != 0 ? curpage - 1: curpage;
             _moveTo(curpage);
         }
     }
@@ -42,10 +50,13 @@ var Scroll = function (ele) {
         return document.body.clientWidth || window.innerWidth;
     }
     function _setX(x) { // 设置x坐标
+        ele_dom.style.transform = 'translateX(' + x + 'px)';
         ele_dom.style.webkitTransform = 'translateX(' + x + 'px)';
+        ele_dom.style.oTransform = 'translateX(' + x + 'px)';
+        ele_dom.style.mozTransform = 'translateX(' + x + 'px)';
     }
     function _moveTo(index) {
-        ele_dom.className += ' scroll-other';
+        ele_dom.className += ' scroll-other'; // 设置时间
         _setX(-index * dwidth);
     }
     this.init = function () { // 初始化
@@ -58,6 +69,7 @@ var Scroll = function (ele) {
             return false;
         }
         dwidth = _getWidth();
+        pageN = ele_dom.childElementCount || ele_dom.children.length; // 滑块数量
         ele_dom.addEventListener('touchstart', that, false);
         ele_dom.addEventListener('touchmove', that, false);
         ele_dom.addEventListener('touchend', that, false);
